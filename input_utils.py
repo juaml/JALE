0dadcbf390b4f662e0dd4624553690cfeadd0b45
Lennart Frahm <l.frahm@fz-juelich.de>
2024-01-30 12:06:06 +0100

    merge coordinate are numbers methods into one function.

--- input_utils.py
+++ input_utils.py
@@ -1,3 +1,4 @@
+import sys
 import pandas as pd
 import numpy as np
 from tal2icbm_spm import tal2icbm_spm
@@ -8,54 +9,28 @@ def load_excel(filepath):
         df = pd.read_excel(filepath)
     except FileNotFoundError:
         print(f"File '{filepath}' not found.")
-        exit()
+        sys.exit()
     except ValueError:
         print(f"Error reading Excel file '{filepath}'. Make sure it's a valid Excel file.")
-        exit()
+        sys.exit()
     except Exception as e:
         print(f"An error occurred: {str(e)}")
-        exit()
+        sys.exit()
     df.dropna(inplace=True, how='all')
     return df
 
-def find_row_indices_with_non_number_values(df, column_name):
-    """
-    Identify row indices in the DataFrame where the specified column contains non-integer values.
-
-    Args:
-    df (pd.DataFrame): The DataFrame to check.
-    column_name (str): The name of the column to check.
-
-    Returns:
-    pd.Index: An Index object containing the row indices with non-integer values in the specified column.
-    """
-    coerced_column = pd.to_numeric(df[column_name], errors='coerce')
-    non_integer_mask = (coerced_column.isnull()) | (coerced_column % 1 != 0)
-    return df.index[non_integer_mask]
-
-def is_column_only_numbers(df, column_name):
-    """
-    Check if all values in the specified column of the Pandas DataFrame are floats.
-
-    Args:
-    df (pd.DataFrame): The DataFrame to check.
-    column_name (str): The name of the column to check.
-
-    Returns:
-    bool: True if all values are floats, False otherwise.
-    """
-    return pd.api.types.is_float_dtype(df[column_name])
-
-def check_coordinates_for_numbers(df):
-    all_true_flag = 1
+def check_coordinates_are_numbers(df):
+    all_coord_numbers_flag = 1
     for coord_col in ['x', 'y', 'z']:
-        coord_col_all_number_bool = is_column_only_numbers(df, coord_col)
+        coord_col_all_number_bool = pd.api.types.is_float_dtype(df[coord_col])
         if coord_col_all_number_bool == False:
-            all_true_flag = 0
-            rows_with_errors = find_row_indices_with_non_number_values(df, coord_col)
+            all_coord_numbers_flag = 0
+            coerced_column = pd.to_numeric(df[coord_col], errors='coerce')
+            non_integer_mask = (coerced_column.isnull()) | (coerced_column % 1 != 0)
+            rows_with_errors = df.index[non_integer_mask]
             print(f'Non-numeric Coordinates in column {coord_col}: {rows_with_errors.values + 2}')
 
-    if all_true_flag == False:
+    if all_coord_numbers_flag == False:
         exit()
     else:
         return df.reset_index(drop=True)
