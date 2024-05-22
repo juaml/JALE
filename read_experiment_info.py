cf017b948f56542a13f70061638c280cf45b8887
Lennart Frahm <l.frahm@fz-juelich.de>
2024-05-22 12:48:24 +0200

    add renaming of columns so that following functions work independently of original names. order still important!

--- read_experiment_info.py
+++ read_experiment_info.py
@@ -4,16 +4,17 @@ import pandas as pd
 import numpy as np
 from input_utils import *
 
+def read_experiment_info(filename):
+    exp_info = load_excel(filepath=filename)
+    exp_info = check_coordinates_are_numbers(exp_info)
+    exp_info = concat_tags(exp_info)
+    exp_info = concat_coordinates(exp_info)
+    exp_info = convert_tal_2_mni(exp_info)
+    exp_info = transform_coordinates_to_voxel_space(exp_info)
+    exp_info = exp_info[['Articles', 'Subjects', 'CoordinateSpace', 'Tags', 'NumberOfFoci', 'Coordinates']]
+    exp_info.to_excel('experiment_info_concat.xlsx', index=False)
 
-filepath = sys.argv[1]
-exp_info = load_excel(filepath=filepath)
-exp_info = check_coordinates_are_numbers(exp_info)
-exp_info = concat_tags(exp_info)
-exp_info = concat_coordinates(exp_info)
-exp_info = convert_tal_2_mni(exp_info)
-exp_info = transform_coordinates_to_voxel_space(exp_info)
-exp_info = exp_info[['Articles', 'Subjects', 'CoordinateSpace', 'Tags', 'NumberOfFoci', 'Coordinates']]
-exp_info.to_excel('experiment_info_concat.xlsx', index=False)
-
-tasks = create_tasks_table(exp_info)
-tasks.to_excel('tasks_info.xlsx', index=False)
+    tasks = create_tasks_table(exp_info)
+    tasks.to_excel('tasks_info.xlsx', index=False)
+    
+    return exp_info, tasks
