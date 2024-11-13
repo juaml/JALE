99911ce1b0356eb156d661506cd1b9d2efb1c22d
Lennart Frahm <l.frahm@fz-juelich.de>
2024-11-13 09:10:13 +0100

    update version in setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="JALE",  # Unique name for your package
-    version="0.1.0",  # Initial version
+    version="0.1.1",  # Initial version
     description="Package allowing users to run Activation Likelihood Estimation Meta-Analysis",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
