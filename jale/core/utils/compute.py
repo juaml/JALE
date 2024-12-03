fix gray matter masking to happen before cluster computation and thresholding to prevent splitting.

jale/core/utils/compute.py
@@ -256,10 +256,11 @@ def compute_clusters(z, cluster_forming_threshold, cfwe_threshold=None):
     """
 
     # Threshold z-values based on the specified cluster threshold
+    z = np.nan_to_num(z)
     sig_arr = (z > norm.ppf(1 - cluster_forming_threshold)).astype(int)
 
     # Find clusters of significant z-values
-    labels, cluster_count = ndimage.label(sig_arr)  # type: ignore
+    labels, cluster_count = ndimage.label(sig_arr)
 
     voxel_count_clusters = np.bincount(labels[labels > 0])
     # Determine the size of the largest cluster (if any clusters exist)

jale/core/utils/contribution.py
@@ -54,7 +54,7 @@ def contribution(
             # Load the ALE results corrected by the current method
             results = load_corrected_results(project_path, exp_name, corr_method)
             if results.any() > 0:  # Proceed only if there are significant results
-                labels, cluster_counts = ndimage.label(results)  # type: ignore
+                labels, cluster_counts = ndimage.label(results)
                 clusters = get_clusters(
                     labels, min_size=5
                 )  # Identify clusters with at least 5 voxels
@@ -135,7 +135,10 @@ def load_corrected_results(project_path, exp_name, corr_method):
             / f"Results/MainEffect/Volumes/{exp_name}_{corr_method}_empty.nii"
         )
         results = loadsave.load(file_path).get_fdata()
-    return np.nan_to_num(results)  # Replace NaNs with zeros for further processing
+
+    # Binarize for clustering
+    results_binary = np.where(results > 0, 1, 0)
+    return np.nan_to_num(results_binary)
 
 
 def get_clusters(labels, min_size=5):